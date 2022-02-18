4c10d359b7c8a777b1f50e1535f85e46991a3cd2
nico <nicogijzenburg@gmail.com>
2022-02-18 23:37:46 +0100

    Clean up, added search, todo products and actaul api calls.

---
+++ admin-ui/stormer.py
@@ -0,0 +1,32 @@
+'''
+This class might seem kinda useless on it owns but has some rationale behind it.
+From the simplegui lib we have "listbox" (and I believe "Combo" as alt. for this) that give the functionality
+ needed to easily display a list. From listbox doc: 
+:param values:                     list of values to display. Can be any type including mixed types as long as they have __str__ method
+:type values:                      List[Any] or Tuple[Any]
+
+it offers one other usefull field namely "metadata of type 'any'". 
+
+So the problem is that I want to display the names of stormers but identify them by vunetID when looking them up in the
+ local cached data or pulling from db (cuz duplicates and such, duh!). Since listbox uses py's build in __str__ having
+  a list of tuples or something similiar is not functional since it will display the ID aswell. 
+I considered adding a sublist to the metadata param and retrieve the id based on 'index' of name in first list. This 
+would have been possible but I didn't like it, it also means that in case of duplicate names I have to extract the correct
+index from name list to get id, I believe this is possible with python but I was not sure.
+
+So my other idea was to just simply overwrite the __str__ method of my list of tuple objects, I figured that it probably would
+be possible but it seemed dirty. Hence this class just so I can have my own nice little str method.
+
+Might start using it to just store all data though in future, this would mean though that all cached user data ends up in the listbox...
+
+'''
+
+class Stormer():
+    def __init__(self, firstName, lastName, vunetId, balance):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.vunetId = vunetId
+        self.balance = balance
+
+    def __str__(self):
+        return self.firstName + ' ' + self.lastName
--- admin-ui/ui.py
+++ admin-ui/ui.py
@@ -1,60 +1,135 @@
-from collections import UserList
-from multiprocessing import dummy
 import PySimpleGUI as sg
 import json
 import pathlib
+from stormer import Stormer
 
-filePath = pathlib.Path(pathlib.Path.cwd(), 'admin-ui/names.json')
-with open(filePath, 'r') as dummyboidatafile:
-    dummyData = json.load(dummyboidatafile)
+def load_dummy_dat():
+    filePath = pathlib.Path(pathlib.Path.cwd(), 'admin-ui/names.json')
+    with open(filePath, 'r') as dummyboidatafile:
+        dummyData = json.load(dummyboidatafile)
 
-print('my data len: ' + str(len(dummyData)))
+    print('dummy data contains ' + str(len(dummyData)) + ' entries.')
+    return dummyData
 
-userList = []
-for user in dummyData:
-    userList.append(user.get('name')) #there properly is a nicer way to do this instead of my noob loop, !todo
+def get_usr_data(inData): #works on temp dummy data, not sure what api calls I get yet!
+    userList = []
 
-leftUsrCol = sg.Col([[sg.Listbox(values=userList, select_mode=sg.SELECT_MODE_EXTENDED, size=(20, 20), key='users')]])
+    for user in inData: #!todo, if time for fun try a list comprehension instead!
+        userList.append(Stormer(user['firstname'], user['lastname'], user['_id'], user['balance']))
 
-#explicitly no middle part for name here for all non dutchies, if data base has names in first, middle, last merge middle and last.
-personalInfo = sg.Col([
-  [sg.Text('Personal info')],
-  [sg.Text('Name:'), sg.Input(default_text = '', size=(25, 1), enable_events=True, key='-FIRSTNAME-')],
-  [sg.Text('Last name:'), sg.Input(default_text = '', size=(25, 1), enable_events=True, key='-LASTNAME-')],
-  [sg.Text('vunetID:'), sg.Input(default_text = '', size=(25, 1), enable_events=True, key='-VUNETID-')],
-  ])
+    userList.sort(key=lambda x: x.__str__()) # performance note (in python, hilarious!) refactor to use operator instead of lambda might* be better 
 
-adminInfo = sg.Col([
-  [sg.Text('Administrative')],
-  [sg.Text('Balance:'), sg.Input(default_text = '', size=(25, 1), enable_events=True, key='-BALANCE-')],
-  ])
+    return userList
 
-rightUsrCol = sg.Column([[personalInfo], [adminInfo]])
+def usr_window(userList):
+    #!todo add refresh btn to reload
 
-userLayout = [[sg.Text('users', font='Any 16')],
+    leftUsrCol = sg.Column([
+      [sg.Listbox(values=userList, select_mode=sg.SELECT_MODE_SINGLE, enable_events=True, size=(50, 20), key='-USERLIST-')],
+      [sg.Text('Search:'), sg.Input(size=(25, 1), focus=True, enable_events=True, key='-FILTER-')]
+      ], element_justification='l', expand_x=True, expand_y=True)
+
+    #explicitly no middle part for name here for all non dutchies, if data base has names in first, middle, last merge middle and last.
+    personalInfo = sg.Col([
+      [sg.Text('Personal info')],
+      [sg.Text('Name:'), sg.Input(default_text = '', size=(25, 1), key='-FIRSTNAME-')],
+      [sg.Text('Last name:'), sg.Input(default_text = '', size=(25, 1), key='-LASTNAME-')],
+      [sg.Text('vunetID:'), sg.Input(default_text = '', size=(25, 1), key='-VUNETID-')],
+    ])
+
+    adminInfo = sg.Col([
+      [sg.Text('Administrative')],
+      [sg.Text('Balance:'), sg.Text('', size=(25, 1), key='-BALANCE-', background_color='white')],
+    ])
+
+    updateBtn = sg.Col([[sg.Submit('Apply Changes', key='-APPLY_CHANGES-')]])
+
+    transactionPanel = sg.Col([
+      [sg.Text('Transaction')],
+      [sg.Text('Change balance by '), sg.Input(size=(8, 1), key='-BALANCE_OPERAND-')],
+      [sg.Text('Comment:'), sg.Multiline(size=(25, 1), key='-TRANSACTION_COMMENT-')],
+      [sg.Submit('Apply Changes', key='-TRANSACTION-')]
+    ])
+
+    rightUsrCol = sg.Column([[personalInfo], [updateBtn], [adminInfo], [transactionPanel]], key='-USR_INFO_PANEL-', visible=False)
+
+    userLayout = [[sg.Text('Stormer people', font='Any 16')],
               sg.vtop([leftUsrCol, rightUsrCol])
               ]
 
-productLayout = [[sg.Text('products')]]
+    return userLayout
 
-tabs = [[
-    sg.TabGroup([[
-        sg.Tab('users', userLayout), sg.Tab('products', productLayout)
-        ]])
-]]
+def main_window(names):
+    sg.theme('SystemDefaultForReal')
+    userLayout = usr_window(names)
+    productLayout = [[sg.Text('products')]]
 
-#Define Window
-window =sg.Window("Tabs", tabs, resizable=True, finalize=True)
+    tabs = [[
+        sg.TabGroup([[
+            sg.Tab('Stormers', userLayout, key='-USERTAB-'), sg.Tab('Products', productLayout, key='-PRODTAB-')
+            ]])
+    ]]
 
-#Read  values entered by user
-while True:
-    event, values = window.read(timeout = 5000, timeout_key = 1) # 5 sec update now for debug, crank up later !todo
-    print('event')
-    print(event)
-    print('values')
-    print(values)
+    #Define Window
+    return sg.Window("Tabs", tabs, resizable=True)
 
-    if event == sg.WIN_CLOSED:
-        break
+def find_user(id, data): #temp function for dummy data, will have to be refactored based on how data is pulled on init and refreshed/loaded on user selection from remote db api
+    for user in data:
+        if user['_id'] == id:
+            return user
 
-window.close()
+    return None
+
+def event_loop(window, data): #!todo functionize these events instead of bunch of stray code, for now hardcoded until using actual api calls
+    completeNameList = get_usr_data(data) #todo, the ui lib adds a threading abstraction for slow/long operations (window.perform_long_operation) which could potentionally be used for async refresh, need to look better into async with the ui in general for this purpose.
+
+    while True:
+        event, values = window.read() #todo think about using elif for events, can there be more than one event per loop? lets assume no
+
+        print('And now on this was the event, ' + str(event) + ' !')
+        #print('values:')
+        #print(values)
+
+        if event == sg.WIN_CLOSED or event == 'none':
+            break
+
+        if event == '-USERLIST-':
+            Stormer = values['-USERLIST-'][0]
+
+            userInfo = find_user(Stormer.vunetId, data)
+
+            window['-FIRSTNAME-'].update(userInfo['firstname'])
+            window['-LASTNAME-'].update(userInfo['lastname'])
+            window['-VUNETID-'].update(userInfo['_id'])
+            window['-BALANCE-'].update(userInfo['balance'])
+            window['-USR_INFO_PANEL-'].update(visible=True)
+
+        if event == '-APPLY_CHANGES-':
+            data = {}
+            data['firstname'] = window['-FIRSTNAME-'].get()
+            data['lastname'] = window['-LASTNAME-'].get()
+            data['vunetid'] = window['-VUNETID-'].get()
+            #data['balance'] = window['-BALANCE-'].get() #if keep, do int check, prolly should not be editable directly though
+
+            print('updating: ' + json.dumps(data))
+
+        if event == '-TRANSACTION-':
+            None #todo start using api calls first
+
+        if event == '-FILTER-':
+            new_list = [i for i in completeNameList if (values['-FILTER-'].lower() in i.__str__().lower() or values['-FILTER-'].lower() in i.vunetId.lower())]
+            window['-USERLIST-'].update(new_list)
+        
+        completeNameList = get_usr_data(data) #todo async or something prolly
+
+    window.close()
+
+def main():
+    data = load_dummy_dat() #will have to figure out a good way of retrieving this, perhaps pull list of names and only get aditional info on selection of name
+    usrData = get_usr_data(data)
+    window = main_window(usrData)
+    event_loop(window, data)
+    print('bye')
+
+if __name__ == "__main__": #python!!!!
+    main()
